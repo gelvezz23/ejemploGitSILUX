fd6229d815abcd8e8f7cc2d13ae52884cd8e3a41
gelvezz23 <gelvezz223@gmail.com>
2020-06-17 16:35:55 -0500

    Modified field of app.js.

--- src/App.js
+++ src/App.js
@@ -7,18 +7,8 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Hola SILUX</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+			<h1>HOLA MUNDO</h1>
+                        <p>Estuve Aquí</p>
 		</div>
 	);
 }
